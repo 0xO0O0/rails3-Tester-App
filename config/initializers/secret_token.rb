e0a18f204d4592a257affac54d84655ae413d6ec
creativeKoder <koder@creativekode.com>
2011-11-02 19:24:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passwordgenarator::Application.config.secret_token = 'c844556449d3302c75913b7fd9ac36f06b15376d78dec21844cd3e1c7fccb355850259850f6c2d8607f1d28a6dfe757ffb6cf9007a9fd496db4d3ca97197793e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Passwordgenarator::Application.config.session_store :cookie_store, key: '_passwordgenarator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Passwordgenarator::Application.config.session_store :active_record_store
